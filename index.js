11d7ede8e831132fa7d2616cc5e5b1b4648af6b7
Linda Hrdinová <hrlin@seznam.cz>
2023-10-24 16:50:02 +0200

    Zpracivání cvičení na doma

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const diceElement = document.querySelector('.dice');
+let side = 1;
 
+const rolling = () => {
+  side = side + 1;
+  if (side === 7) {
+    side = 1;
+  }
+  diceElement.src = `img/side${side}.svg`;
+};
+
+document.addEventListener('keydown', rolling);
